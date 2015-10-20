1c690beb3106fa2f0b751f25d12942e4d5f4ad9f
CrazyGuo <guohuanhuan_cool@126.com>
2015-10-20 09:07:59 +0800

    Signed-off-by: CrazyGuo <guohuanhuan_cool@126.com>

---
+++ X-Cust File Upload/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using X_Cust_File_Upload.Helpers;
+
+namespace X_Cust_File_Upload.Controllers
+{
+    [CustomAuthorize(Roles="T1")]
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetUniqueIdentifier()
+        {
+            return Json(Guid.NewGuid().ToString());
+        }
+
+        [HttpPost]
+        public ActionResult SingleFileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MultipleFileUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoUploadSingleFile(HttpPostedFileBase berkas, string guid)
+        {
+            bool result = false;
+            string filePath = Server.MapPath("~/Temporary/") + berkas.FileName;
+
+            int fileLength = berkas.ContentLength;
+            HttpContext.Cache[guid + "_total"] = fileLength;
+            byte[] fileContent = new byte[fileLength];
+            int bufferLength = 5 * 1024;
+            byte[] buffer = new byte[bufferLength];
+            int bytesRead = 0;
+
+            FileStream outputFileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite); 
+            using (Stream inputFileStream = berkas.InputStream)
+            {
+                while ((bytesRead = inputFileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    outputFileStream.Write(buffer, 0, bytesRead);
+                    outputFileStream.Flush();
+
+                    HttpContext.Cache[guid + "_current"] = Convert.ToInt32(HttpContext.Cache[guid + "_current"]) + bytesRead;
+                    Debug.WriteLine(HttpContext.Cache[guid + "_current"].ToString());
+                    Thread.Sleep(50);
+                }
+
+                inputFileStream.Close();
+                inputFileStream.Dispose();
+            }
+
+            outputFileStream.Close();
+            outputFileStream.Dispose();
+            result = true;
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public ActionResult TrackProgress(string guid)
+        {
+            try
+            {
+                double paramCurrentFileSize = Convert.ToDouble(HttpContext.Cache[guid + "_current"]);
+                double paramTotalFileSize = Convert.ToDouble(HttpContext.Cache[guid + "_total"]);
+                int uploadProgress = Convert.ToInt32(paramCurrentFileSize * 100 / paramTotalFileSize);
+
+                return Json(uploadProgress);
+            }
+            catch (Exception)
+            {
+                return Json(0);
+            }
+        }
+    }
+}
---
+++ X-Cust File Upload/Helpers/CustomAuthorizeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace X_Cust_File_Upload.Helpers
+{
+
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            
+            if (httpContext == null)
+            {
+                throw new ArgumentNullException("httpContext");
+            }
+            if (!httpContext.User.Identity.IsAuthenticated)//未登录的话 跳转到登录界面
+                return false;
+            return true; 
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("/Auth/LogOn");
+        }
+    }
+}
